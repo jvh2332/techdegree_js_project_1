8264c5257a5d84462edaf21ae63bceb4a909e5ca
jvh2332 <jvh2332@users.noreply.github.com>
2024-11-21 16:14:41 +0100

    added print function to print the code in the html

--- a_random_quote_generator-v1/js/script.js
+++ a_random_quote_generator-v1/js/script.js
@@ -4,57 +4,66 @@ project 1 - A Random Quote Generator
 ******************************************/
 
 // For assistance: 
-  // Check the "Project Resources" section of the project instructions
-  // Reach out in your Slack community - https://treehouse-fsjs-102.slack.com/app_redirect?channel=chit-chat
+// Check the "Project Resources" section of the project instructions
+// Reach out in your Slack community - https://treehouse-fsjs-102.slack.com/app_redirect?channel=chit-chat
 
-/*** 
- * `quotes` array 
-***/
+//An array of quote objects
 
 let quotes = [
-  {quote : "Be yourself; everyone else is already taken.",
-  source : "Oscar Wilde"
+  {
+    quote: "Be yourself; everyone else is already taken.",
+    source: "Oscar Wilde"
   },
-  {quote : "Whenever you feel like criticizing any one...just remember that all the people in this world haven't had the advantages that you've had.",
-  source : "F. Scott Fitzgerald",
-  citation : "The Great Gatsby",
-  year : "1925"
-  },  
-  {quote : "She burned too bright for this world.",
-  source : "Emily Bronte",
-  citation : "Wuthering Heights"
+  {
+    quote: "Whenever you feel like criticizing any one... just remember that all the people in this world haven't had the advantages that you've had.",
+    source: "F. Scott Fitzgerald",
+    citation: "The Great Gatsby",
+    year: "1925"
   },
-  {quote : "For all evils there are two remedies - time and silence.",
-  source : "Alexandre Dumas",
-  citation : "The Count of Monte Cristo",
-  year : "1844"
+  {
+    quote: "She burned too bright for this world.",
+    source: "Emily Bronte",
+    citation: "Wuthering Heights"
   },
-  {quote : "Unable are the loved to die for love is immortality.",
-  source : "Emily Dickinson"
+  {
+    quote: "For all evils there are two remedies - time and silence.",
+    source: "Alexandre Dumas",
+    citation: "The Count of Monte Cristo",
+    year: "1844"
   },
-  {quote : "We are all broken, that's how the light gets in.",
-  source : "Ernest Hemingway"
+  {
+    quote: "Unable are the loved to die for love is immortality.",
+    source: "Emily Dickinson"
+  },
+  {
+    quote: "We are all broken, that's how the light gets in.",
+    source: "Ernest Hemingway"
   }
 ];
 
-/***
- * `getRandomQuote` function
-***/
+//A function that returns a random quote from the provided array of quote objects
 
-function getRandomQuote (arr) {
-  const randomNumber = Math.floor( Math.random() * arr.length );
-  let randomQuote = arr[randomNumber]; 
+function getRandomQuote(arr) {
+  const randomNumber = Math.floor(Math.random() * arr.length);
+  let randomQuote = arr[randomNumber];
   return randomQuote;
 }
 
-/***
- * `printQuote` function
-***/
+//A function that displays the random quote from the quotes array in the html
 
-function printQuote (arr) {
-  const randomQuote = getRandomQuote(arr);
-  let html = `<p class="quote"> A random quote </p>`
-  `<p class="source">quote source </p>`
+function printQuote() {
+  const randomQuote = getRandomQuote(quotes);
+  let html = `<p class="quote"> ${randomQuote.quote}</p>
+              <p class="source">${randomQuote.source}`;
+
+  if (randomQuote.citation) {
+    html += `<span class="citation">${randomQuote.citation}</span>`;
+  }
+  if (randomQuote.year) {
+    html += `<span class="year">${randomQuote.year}</span>`;
+  }
+  html += `</p>`
+  document.getElementById('quote-box').innerHTML = html;
 }
 
 
@@ -63,4 +72,4 @@ function printQuote (arr) {
  * DO NOT CHANGE THE CODE BELOW!!
 ***/
 
-//document.getElementById('load-quote').addEventListener("click", printQuote, false);
+document.getElementById('load-quote').addEventListener("click", printQuote, false);
